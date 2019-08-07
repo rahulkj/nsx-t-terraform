9f0669d883b86681b42843f48858b1ad0a5953e6
Rahul Jain <rahulkj@gmail.com>
2019-08-07 08:13:31 -0400

    Reformatting terraform to work with 0.12.0 cli

--- main.tf
+++ main.tf
@@ -1,80 +1,81 @@
 provider "nsxt" {
-  host                     = "${var.nsx_hostname}"
-  username                 = "${var.nsx_user}"
-  password                 = "${var.nsx_password}"
-  allow_unverified_ssl     = true
-  max_retries              = 10
-  retry_min_delay          = 500
-  retry_max_delay          = 5000
-  retry_on_status_codes    = [429]
+  host                  = var.nsx_hostname
+  username              = var.nsx_user
+  password              = var.nsx_password
+  allow_unverified_ssl  = true
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
 }
 
 module "pas" {
   source = "./modules/pas"
 
-  env_name                      = "${var.env_name}"
+  env_name = var.env_name
 
-  edge_cluster_name             = "${var.edge_cluster_name}"
-  transport_zone_vlan_name      = "${var.transport_zone_vlan_name}"
-  transport_zone_overlay_name   = "${var.transport_zone_overlay_name}"
+  edge_cluster_name           = var.edge_cluster_name
+  transport_zone_vlan_name    = var.transport_zone_vlan_name
+  transport_zone_overlay_name = var.transport_zone_overlay_name
 
-  pcf_ip_block_name             = "${var.pcf_ip_block_name}"
-  pcf_ip_block_cidr             = "${var.pcf_ip_block_cidr}"
-  pas_snat_ip_pool_name         = "${var.pas_snat_ip_pool_name}"
-  pas_snat_ip_pool_range        = "${var.pas_snat_ip_pool_range}"
-  pas_snat_ip_pool_cidr         = "${var.pas_snat_ip_pool_cidr}"
+  pcf_ip_block_name      = var.pcf_ip_block_name
+  pcf_ip_block_cidr      = var.pcf_ip_block_cidr
+  pas_snat_ip_pool_name  = var.pas_snat_ip_pool_name
+  pas_snat_ip_pool_range = var.pas_snat_ip_pool_range
+  pas_snat_ip_pool_cidr  = var.pas_snat_ip_pool_cidr
 
-  infrastructure_ls             = "${var.infrastructure_ls}"
-  pas_ls                        = "${var.pas_ls}"
-  services_ls                   = "${var.services_ls}"
+  infrastructure_ls = var.infrastructure_ls
+  pas_ls            = var.pas_ls
+  services_ls       = var.services_ls
 
-  infrastructure_subnet_cidr    = "${var.infrastructure_subnet_cidr}"
-  pas_subnet_cidr               = "${var.pas_subnet_cidr}"
-  services_subnet_cidr          = "${var.services_subnet_cidr}"
-  router_t0                     = "${var.router_t0}"
-  uplink_router_ip              = "${var.uplink_router_ip}"
-  static_route_next_hop_ip      = "${var.static_route_next_hop_ip}"
+  infrastructure_subnet_cidr = var.infrastructure_subnet_cidr
+  pas_subnet_cidr            = var.pas_subnet_cidr
+  services_subnet_cidr       = var.services_subnet_cidr
+  router_t0                  = var.router_t0
+  uplink_router_ip           = var.uplink_router_ip
+  static_route_next_hop_ip   = var.static_route_next_hop_ip
 
-  loadbalancer_type             = "${var.loadbalancer_type}"
+  loadbalancer_type = var.loadbalancer_type
 
-  router_server_pool_name       = "${var.router_server_pool_name}"
-  diego_brain_server_pool_name  = "${var.diego_brain_server_pool_name}"
-  istio_server_pool_name        = "${var.istio_server_pool_name}"
+  router_server_pool_name      = var.router_server_pool_name
+  diego_brain_server_pool_name = var.diego_brain_server_pool_name
+  istio_server_pool_name       = var.istio_server_pool_name
 
-  ops_manager_private_ip        = "${var.ops_manager_private_ip}"
-  opsmanager_public_ip          = "${var.opsmanager_public_ip}"
-  pas_routers_public_ip         = "${var.pas_routers_public_ip}"
-  pas_diego_brains_public_ip    = "${var.pas_diego_brains_public_ip}"
-  pas_istio_public_ip           = "${var.pas_istio_public_ip}"
+  ops_manager_private_ip     = var.ops_manager_private_ip
+  opsmanager_public_ip       = var.opsmanager_public_ip
+  pas_routers_public_ip      = var.pas_routers_public_ip
+  pas_diego_brains_public_ip = var.pas_diego_brains_public_ip
+  pas_istio_public_ip        = var.pas_istio_public_ip
 
-  snat_public_ip                = "${var.snat_public_ip}"
-  snat_cidr                     = "${var.snat_cidr}"
+  snat_public_ip = var.snat_public_ip
+  snat_cidr      = var.snat_cidr
 }
 
 module "pks" {
   source = "./modules/pks"
 
-  env_name                      = "${var.env_name}"
+  env_name = var.env_name
 
-  edge_cluster_name             = "${var.edge_cluster_name}"
-  transport_zone_vlan_name      = "${var.transport_zone_vlan_name}"
-  transport_zone_overlay_name   = "${var.transport_zone_overlay_name}"
+  edge_cluster_name           = var.edge_cluster_name
+  transport_zone_vlan_name    = var.transport_zone_vlan_name
+  transport_zone_overlay_name = var.transport_zone_overlay_name
 
-  router_t0                     = "${module.pas.router_t0}"
+  router_t0 = module.pas.router_t0
 
-  pks_snat_ip_pool_name         = "${var.pks_snat_ip_pool_name}"
-  pks_snat_ip_pool_range        = "${var.pks_snat_ip_pool_range}"
-  pks_snat_ip_pool_cidr         = "${var.pks_snat_ip_pool_cidr}"
+  pks_snat_ip_pool_name  = var.pks_snat_ip_pool_name
+  pks_snat_ip_pool_range = var.pks_snat_ip_pool_range
+  pks_snat_ip_pool_cidr  = var.pks_snat_ip_pool_cidr
 
-  pks_ls                        = "${var.pks_ls}"
-  pks_subnet_cidr               = "${var.pks_subnet_cidr}"
+  pks_ls          = var.pks_ls
+  pks_subnet_cidr = var.pks_subnet_cidr
 
-  pks_api_private_ip            = "${var.pks_api_private_ip}"
-  pks_public_ip                 = "${var.pks_public_ip}"
+  pks_api_private_ip = var.pks_api_private_ip
+  pks_public_ip      = var.pks_public_ip
 
-  pks_pods_ip_block_name        = "${var.pks_pods_ip_block_name}"
-  pks_pods_ip_block_cidr        = "${var.pks_pods_ip_block_cidr}"
+  pks_pods_ip_block_name = var.pks_pods_ip_block_name
+  pks_pods_ip_block_cidr = var.pks_pods_ip_block_cidr
 
-  pks_nodes_ip_block_name       = "${var.pks_nodes_ip_block_name}"
-  pks_nodes_ip_block_cidr       = "${var.pks_nodes_ip_block_cidr}"
+  pks_nodes_ip_block_name = var.pks_nodes_ip_block_name
+  pks_nodes_ip_block_cidr = var.pks_nodes_ip_block_cidr
 }
+
--- variables.tf
+++ variables.tf
@@ -1,62 +1,131 @@
-variable "env_name" {}
+variable "env_name" {
+}
 
-variable "nsx_hostname" {}
-variable "nsx_user" {}
-variable "nsx_password" {}
+variable "nsx_hostname" {
+}
 
-variable "edge_cluster_name" {}
-variable "transport_zone_vlan_name" {}
-variable "transport_zone_overlay_name" {}
+variable "nsx_user" {
+}
+
+variable "nsx_password" {
+}
+
+variable "edge_cluster_name" {
+}
+
+variable "transport_zone_vlan_name" {
+}
+
+variable "transport_zone_overlay_name" {
+}
 
 variable "pcf_ip_block_name" {
-  type = "string"
+  type    = string
   default = "pcf_ip_block"
 }
 
-variable "pcf_ip_block_cidr" {}
+variable "pcf_ip_block_cidr" {
+}
 
-variable "pas_snat_ip_pool_name" {}
-variable "pas_snat_ip_pool_range" {}
-variable "pas_snat_ip_pool_cidr" {}
+variable "pas_snat_ip_pool_name" {
+}
 
-variable "pks_snat_ip_pool_name" {}
-variable "pks_snat_ip_pool_range" {}
-variable "pks_snat_ip_pool_cidr" {}
+variable "pas_snat_ip_pool_range" {
+}
 
-variable "infrastructure_ls" {}
-variable "pas_ls" {}
-variable "services_ls" {}
-variable "pks_ls" {}
+variable "pas_snat_ip_pool_cidr" {
+}
 
-variable "infrastructure_subnet_cidr" {}
-variable "pas_subnet_cidr" {}
-variable "services_subnet_cidr" {}
-variable "pks_subnet_cidr" {}
+variable "pks_snat_ip_pool_name" {
+}
 
-variable "router_t0" {}
-variable "uplink_router_ip" {}
-variable "static_route_next_hop_ip" {}
+variable "pks_snat_ip_pool_range" {
+}
 
-variable "loadbalancer_type" {}
+variable "pks_snat_ip_pool_cidr" {
+}
 
-variable "router_server_pool_name" {}
-variable "diego_brain_server_pool_name" {}
-variable "istio_server_pool_name" {}
+variable "infrastructure_ls" {
+}
 
-variable "ops_manager_private_ip" {}
-variable "opsmanager_public_ip" {}
-variable "pas_routers_public_ip" {}
-variable "pas_diego_brains_public_ip" {}
-variable "pas_istio_public_ip" {}
+variable "pas_ls" {
+}
 
-variable "snat_public_ip" {}
-variable "snat_cidr" {}
+variable "services_ls" {
+}
 
-variable "pks_api_private_ip" {}
-variable "pks_public_ip" {}
+variable "pks_ls" {
+}
 
-variable "pks_pods_ip_block_name" {}
-variable "pks_pods_ip_block_cidr" {}
+variable "infrastructure_subnet_cidr" {
+}
 
-variable "pks_nodes_ip_block_name" {}
-variable "pks_nodes_ip_block_cidr" {}
+variable "pas_subnet_cidr" {
+}
+
+variable "services_subnet_cidr" {
+}
+
+variable "pks_subnet_cidr" {
+}
+
+variable "router_t0" {
+}
+
+variable "uplink_router_ip" {
+}
+
+variable "static_route_next_hop_ip" {
+}
+
+variable "loadbalancer_type" {
+}
+
+variable "router_server_pool_name" {
+}
+
+variable "diego_brain_server_pool_name" {
+}
+
+variable "istio_server_pool_name" {
+}
+
+variable "ops_manager_private_ip" {
+}
+
+variable "opsmanager_public_ip" {
+}
+
+variable "pas_routers_public_ip" {
+}
+
+variable "pas_diego_brains_public_ip" {
+}
+
+variable "pas_istio_public_ip" {
+}
+
+variable "snat_public_ip" {
+}
+
+variable "snat_cidr" {
+}
+
+variable "pks_api_private_ip" {
+}
+
+variable "pks_public_ip" {
+}
+
+variable "pks_pods_ip_block_name" {
+}
+
+variable "pks_pods_ip_block_cidr" {
+}
+
+variable "pks_nodes_ip_block_name" {
+}
+
+variable "pks_nodes_ip_block_cidr" {
+}
+
