e96c52ea04bcdc81202ef09b9d96d0ad83c28033
rahulkj <rahulkj@gmail.com>
2019-08-12 19:08:33 -0500

    Fixing the ip-pools script

--- modules/pks/ip-pools.tf
+++ modules/pks/ip-pools.tf
@@ -2,7 +2,7 @@ resource "nsxt_ip_pool" "pks_snat_ip_pool" {
   description = "pks_snat_ip_pool provisioned by Terraform"
   display_name = "${var.pks_snat_ip_pool_name}"
 
-  subnet = {
+  subnet {
     allocation_ranges = ["${var.pks_snat_ip_pool_range}"]
     cidr              = "${var.pks_snat_ip_pool_cidr}"
   }
